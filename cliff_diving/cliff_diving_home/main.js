1a13e9dddf231e3ac4fbababf928186152fa00a6
ThomasTraineau <thomas_traineau@tutanota.com>
2021-06-05 11:21:23 +0200

    updates

--- cliff_diving/cliff_diving_home/main.js
+++ cliff_diving/cliff_diving_home/main.js
@@ -5,12 +5,6 @@
 
 
 
-
-
-
-
-
-
 //creating the map variable
 var mymap = L.map('mapid').setView([48.85, 2.35], 10);
 
@@ -46,21 +40,34 @@ var spotInfoContainer = document.getElementById("testtttt");
 
 
 //displays the spot's info (description, pics etc...) on the current page
-function displaySpotInfo(name, description, pictureAmount) {
+function displaySpotInfo(name, pictureAmount) {
   spotName = "<div class='spotNameTitle'>"+name+"</div>";
-  spotDescription = "<div class='spotDescription'>"+description+"</div>";
-  spotPictures = "<div class='spotPictures'>"
-  picturesList = pictures.split(','); //because the function addSpotsToMap gives a long string as an argument
-  for(i=0; i<picturesList.length; i++){
-    spotPictures += "<img src='"+picturesList[i]+"' width='400px' alt=''>"
+
+  spotPictures = "<div class='spotPictures'>";
+
+  for(i=0; i<pictureAmount; i++){
+    spotPictures += "<img src='../cliff_diving_spots/"+name+"/picture"+i+".jpg' width='400px' alt=''>"
   }
   spotPictures += "</div>";
-  spotInfoContainer.insertAdjacentHTML("beforeend", spotName);
-  spotInfoContainer.insertAdjacentHTML("beforeend", spotDescription);
-  spotInfoContainer.insertAdjacentHTML("beforeend", '<div style="margin-top:20px;"></div>')
+  console.log(spotPictures)
+  spotInfoContainer.insertAdjacentHTML("beforeend", "<p>"+spotName+"</p>");
+  displaySpotDescription(name);
+  spotInfoContainer.insertAdjacentHTML("beforeend", '<div style="margin-top:100px;"></div>');
   spotInfoContainer.insertAdjacentHTML("beforeend", spotPictures);
-  console.log(spotPictures);
 }
+
+
+//function to request display the spot description when clicked on
+function displaySpotDescription(name) {
+  var descriptionRequest = new XMLHttpRequest();
+  descriptionRequest.open('GET', 'https://thomastraineau.github.io/Outdoor-spots/cliff_diving/cliff_diving_spots/'+name+'/description.txt');
+  descriptionRequest.onload = function() {
+    var description = descriptionRequest.responseText;
+    spotInfoContainer.insertAdjacentHTML("beforeend", "<p>"+description+"</p>");
+  };
+  descriptionRequest.send();
+}
+
 
 
 //Getting the spots from the json file
